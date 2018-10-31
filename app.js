1ced09f3b99e9ffb0ad469268afe97052aa21133
adriangc15 <adrian.chavarria.alvarez@est.una.ac.cr>
2018-10-30 20:09:22 -0600

    Iniciando la aplicacion

---
+++ app.js
@@ -0,0 +1,9 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        noticias: [
+            { titulo: 'Mi primer noticia', contenido: 'Todo el contenido de la noticia...' },
+            { titulo: 'Mi segunda noticia', contenido: 'Todo el contenido de la segunda noticia...' }
+        ]
+    }
+})
